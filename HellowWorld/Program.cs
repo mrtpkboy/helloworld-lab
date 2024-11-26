820466e13b37bcf8dedc21406d474958fe20159c
muffyn_pp <mrtpkboy@gmail.com>
2024-11-26 11:30:20 +0700

    add new project

---
+++ HellowWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Tapakhon Khosawattanakul";
+string student_id = "673150188-1";
+// string is Datatype -> ชนิดของข้อมูล
+// name is value of varible
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My student id: 673450188-1");
